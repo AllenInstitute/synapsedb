627beeeaef3726d1b57dde18e812f5ede64b13e7
Forrest Collman <forrest.collman@gmail.com>
2018-05-30 15:38:49 -0700

    adjustment

--- synapsedb/ratings/controllers.py
+++ synapsedb/ratings/controllers.py
@@ -26,7 +26,7 @@ def get_ratingsources_of_object(object_id):
     return jsonify([result[0] for result in results])
 
 
-@mod_ratings.route("/ratings_of/<object_id>/by/<ratingsource_id>/")
+@mod_ratings.route("/ratings_of/<object_id>/ratingsource/<ratingsource_id>/")
 def get_ratings_of_object_by_source(ratingsource_id, object_id):
     ratings = Rating.query.filter_by(rating_source_id=ratingsource_id,
                                      object_id=object_id)
