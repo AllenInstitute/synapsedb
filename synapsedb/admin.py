76ad87513f456dc9280c0334954633e9001477cd
Forrest Collman <forrest.collman@gmail.com>
2018-06-03 16:27:20 -0700

    added SegmentationChannel to admin

--- synapsedb/admin.py
+++ synapsedb/admin.py
@@ -1,6 +1,6 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from synapsedb.volumes.models import Volume, ImageChannel, DataSet, VolumeLink
+from synapsedb.volumes.models import Volume, ImageChannel, DataSet, VolumeLink, SegmentationChannel
 from synapsedb.synapses.models import SynapseCollection, Synapse
 from synapsedb.ratings.models import RatingSource, ClassificationType
 
@@ -14,10 +14,14 @@ def setup_admin(app, db):
     admin.add_view(ModelView(DataSet, db.session, category='Volumes'))
     admin.add_view(ModelView(Volume, db.session, category='Volumes'))
     admin.add_view(NoTypeView(ImageChannel, db.session, category='Volumes'))
+    admin.add_view(NoTypeView(SegmentationChannel,
+                              db.session,
+                              category='Volumes'))
     admin.add_view(NoTypeView(VolumeLink, db.session, category='Volumes'))
     admin.add_view(NoTypeView(SynapseCollection,
                               db.session, category='Synapses'))
     admin.add_view(NoTypeView(Synapse, db.session, category='Synapses'))
     admin.add_view(ModelView(RatingSource, db.session, category='Ratings'))
-    admin.add_view(ModelView(ClassificationType, db.session, category='Ratings'))
+    admin.add_view(ModelView(ClassificationType,
+                             db.session, category='Ratings'))
     return admin
