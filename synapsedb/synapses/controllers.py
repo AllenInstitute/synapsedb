4758e5cdc7d1992c35cc5cf161977e1f30a05eaf
Forrest Collman <forrest.collman@gmail.com>
2018-06-07 17:02:39 -0700

    added table with all synapses to collection view

--- synapsedb/synapses/controllers.py
+++ synapsedb/synapses/controllers.py
@@ -9,8 +9,10 @@ from synapsedb.ratings.controllers import get_rating_summary_df
 # from geoalchemy2 import Geometry
 # from geoalchemy2.shape import from_shape, to_shape
 from synapsedb.volumes.models import VolumeLink
-from synapsedb.synapses.postgis import Box3D
+from synapsedb.synapses.postgis import Box3D, ST_XMin, ST_YMin, ST_ZMin, ST_XMax, ST_YMax, ST_ZMax
 from synapsedb.synapses.schemas import SynapseSchema
+from functools import partial
+import numpy as np
 import ndviz
 try:
     from urllib import parse
@@ -40,9 +42,10 @@ def index():
                              '.view_synapsecollection')
     return render_template('table.html', table=df.to_html(escape=False))
 
+
 @mod_synapses.route("/synapsecollections")
 def get_synapsecollections():
-    collection= SynapseCollection.query.all()
+    collection = SynapseCollection.query.all()
     schema = SynapseCollectionSchema(exclude=['objects'])
     return schema.jsonify(collection, many=True)
 
@@ -52,9 +55,10 @@ def view_synapse_by_collection_oid(id, oid):
     synapse = Synapse.query.filter_by(object_collection_id=id, oid=oid).first()
     return view_synapse(synapse.id)
 
+
 @mod_synapses.route("/synapsecollection/<id>")
 def get_synapsecollection(id):
-    collection= SynapseCollection.query.filter_by(id=id).first()
+    collection = SynapseCollection.query.filter_by(id=id).first()
     schema = SynapseCollectionSchema()
     return schema.jsonify(collection)
 
@@ -62,10 +66,31 @@ def get_synapsecollection(id):
 @mod_synapses.route("/view/synapsecollection/<id>")
 def view_synapsecollection(id):
     collection = SynapseCollection.query.filter_by(id=id).first_or_404()
-    # df = named_objects_to_df(collections,
-    #                          SynapseCollectionSchema(),
-    #                          '.view_synapsecollection')
-    return render_template('synapse_collection.html', collection=collection)
+    query = Synapse.query.with_entities(Synapse.id,
+                                        Synapse.oid,
+                                        (ST_XMin(Synapse.areas) +
+                                         ST_XMax(Synapse.areas)) / 2,
+                                        ST_YMin(Synapse.areas) +
+                                        ST_YMax(Synapse.areas) / 2,
+                                        ST_ZMin(Synapse.areas) +
+                                        ST_ZMax(Synapse.areas) / 2,
+                                        ).filter(Synapse.object_collection_id == id)
+    df = pd.read_sql(query.statement, db.session.bind)
+    for k, row in df.iterrows():
+        url = make_synapse_link_fast(
+            collection.link, [row.anon_1, row.anon_2, row.anon_3])
+        if url is not None:
+            df.loc[k, 'link'] = "<a href='{}'>vizlink</a>".format(url)
+        else:
+            df.loc[k, 'link'] = ""
+    
+    df['id'] = df.id.map(lambda x: "<a href='{}'>{}</a>"
+                         .format(url_for('.view_synapse', id=x), x))
+    df_cut = df[['id', 'oid', 'link']]
+    return render_template('synapse_collection.html',
+                           collection=collection,
+                           table=df_cut.to_html(index=False,
+                                                escape=False))
 
 
 def get_box_as_array(box3d):
@@ -90,29 +115,42 @@ def view_synapse_json(id):
     return schema.jsonify(synapse)
 
 
-@mod_synapses.route("/view/synapse/<id>")
-def view_synapse(id):
-    synapse = Synapse.query.filter_by(id=id)[0]
-    # shape = to_shape(synapse.areas)
-    # print(type(synapse.areas.ST_GeometricMedian()))
-    box3d = Box3D(synapse.areas)
-    center_box = get_box_center(box3d)
-    vid = synapse.collection.volume_id
-
-    rating_df = get_rating_summary_df(synapse.id)
-    if rating_df is not None:
-        rating_html = rating_df.to_html(escape=False)
-    else:
-        rating_html = None
-    link = synapse.collection.link
+def make_synapse_link_fast(link, center_box):
     if link is not None:
         urlp = parse.urlparse(link.link)
         state = ndviz.parse_url(link.link)
         state.voxel_coordinates = center_box
         nglancprefix = "{}://{}".format(urlp.scheme, urlp.netloc)
         url = ndviz.to_url(state, prefix=nglancprefix)
+        return url
     else:
-        url = None
+        return None
+
+
+def make_synapse_link(synapse, center_box=None):
+    link = synapse.collection.link
+    if (center_box is None):
+        box3d = Box3D(synapse.areas)
+        center_box = get_box_center(box3d)
+    url = make_synapse_link_fast(link, center_box)
+    return url
+
+
+@mod_synapses.route("/view/synapse/<id>")
+def view_synapse(id):
+    synapse = Synapse.query.filter_by(id=id)[0]
+    # shape = to_shape(synapse.areas)
+    # print(type(synapse.areas.ST_GeometricMedian()))
+
+    box3d = Box3D(synapse.areas)
+    center_box = get_box_center(box3d)
+
+    rating_df = get_rating_summary_df(synapse.id)
+    if rating_df is not None:
+        rating_html = rating_df.to_html(escape=False)
+    else:
+        rating_html = None
+    url = synapse.make_link(center_box)
     # print(to_shape(centroid))
     return render_template("synapse.html",
                            center=center_box,
